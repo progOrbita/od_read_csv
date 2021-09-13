79e692478fe89eabad3e4d3162c8bfae4f96349d
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-09-13 10:57:50 +0200

    check for csv file after being readable and lines changed

--- src/ReadCsv.php
+++ src/ReadCsv.php
@@ -1,26 +1,29 @@
 <?php
 
-declare (strict_types=1);
+declare(strict_types=1);
 
 namespace OrbitaDigital\Read;
 
-class ReadCsv{
+class ReadCsv
+{
     /**
      * Read a file and returns the array with the data.
      * Open the filestream, r -> read mode only.
      * fgetcsv requires de file, line length to read and separator. Will return false at the end of the file
      * Save all the rows onto the array which is returned
      */
-    public static function readCSV(string $fileStream){
-        //If file isn't a csv.
-        if(!is_readable($fileStream) && preg_match('/.+\.csv/',$fileStream)){
-            return false;
+    public static function readCSV(string $fileStream)
+    {
         if (!file_exists($fileStream)) {
             return "<b>" . $fileStream . "</b> doesn't exist<br/>";
         }
         if (!is_readable($fileStream)) {
             return "<b>" . $fileStream . "</b> couldn't be read<br/>";
         }
+        if (preg_match('/^.+\.csv/i', $fileStream) <= 0) {
+            return "<b>" . $fileStream . "</b> isn't a .csv file";
+        }
+
         $file = fopen($fileStream, 'r');
         $resultArr = [];
         $headArr = [];
@@ -33,3 +36,4 @@ class ReadCsv{
         }
         return $resultArr;
     }
+}
