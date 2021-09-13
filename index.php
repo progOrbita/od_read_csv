b51b3baa9345830df5d52924c4a99bd5ab460006
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-09-13 10:29:23 +0200

    Merge csv arrays and remove recursive

--- index.php
+++ index.php
@@ -17,12 +17,16 @@ $ptArr = Resources::processCsv('data_pt.csv');
 $aaArr = Resources::processCsv('data_e.csv');
 $write = Resources::processCsv('write_only.json');
 $noncsv = Resources::processCsv('readme.md');
+
+//Merge the arrays
+$merge = array_merge($enArr,$frArr,$esArr,$ptArr);
+
+$data = [];
+
 foreach ($merge as $key => $value) {
     $data[$value['Id']]['Titulo'][] = $value['Titulo'];
     $data[$value['Id']]['Description'][] = $value['Description'];
     
 }
-//Merge the E1/E2/E3 fields
-$arrayJson = array_merge_recursive($masterArray[0],$masterArray[1],$masterArray[2],$masterArray[3],$masterArray[4],$masterArray[5]);
 $saveJson = json_encode($arrayJson);
 file_put_contents('fileJson.json',$saveJson);
