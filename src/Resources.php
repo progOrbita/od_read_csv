808185aca2581751aad4b1f42d915d08dacb9877
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-09-13 16:00:32 +0200

    data check to Resources function

--- src/Resources.php
+++ src/Resources.php
@@ -3,8 +3,34 @@
 namespace OrbitaDigital\Read;
 
 class Resources{
-
-    
+    /**
+     * Write data to a file
+     * @param array $data array containing all the information
+     * @param string $file Name of the file to be created and inserted the data
+     * @param string $dir Optional, folder where to insert the file
+     * @return string A message showing the result
+     */
+    public static function dataToJsonFile($data,$file,$dir=null){
+        $jsonData = json_encode($data,JSON_PRETTY_PRINT);
+        $date = date('d_F_Y_H_i_s'); //day, month in letters, year 4 digits, 24hour_mins_seconds
+        if($dir==null){
+            //default route when no directory is sent, where script is executed.
+            $dir = getcwd();
+        }
+        $file = $dir.'/data_'.$date.'.json';
+        if(!is_dir($dir)){
+            return 'Directory not found';
+        }
+        else if(!is_file($file)){
+            echo '<br/>File dont exist, creating '.$file.' ...';
+            $createdFile = fopen($file,'w');
+            fclose($createdFile);
+        }
+        else if(!is_writable($file)){
+            return 'Information cant be written';
+        }
+        file_put_contents($file,$jsonData);
+        return "<br/>Data inserted in file: ".$file;
 }
 
 
