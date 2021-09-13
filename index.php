247ec5f7d8f7eb280560078f5423e0cd86f274c4
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-09-13 10:28:18 +0200

    using foreach to create the array from for

--- index.php
+++ index.php
@@ -10,22 +10,6 @@ $ptArr = [];
 $frArr = [];
 $masterArray = [];
 
-
-$fileNumber = 4;
-
-for ($i=1; $i < $fileNumber; $i++) { 
-    for ($j=1; $j < $fileNumber-1; $j++) {
-        for ($k=1; $k < $fileNumber-2; $k++) { 
-            $masterArray[][$esArr[$i][0]] = [
-                    $esArr[0][$j] => [
-                        $enArr[$i][$j],
-                        $frArr[$i][$j],
-                        $esArr[$i][$j],
-                        $ptArr[$i][$j]
-                    ]
-                ];
-        }
-    }
 $enArr = Resources::processCsv('data_en.csv');
 $frArr = Resources::processCsv('data_fr.csv');
 $esArr = Resources::processCsv('data_es.csv');
@@ -33,6 +17,10 @@ $ptArr = Resources::processCsv('data_pt.csv');
 $aaArr = Resources::processCsv('data_e.csv');
 $write = Resources::processCsv('write_only.json');
 $noncsv = Resources::processCsv('readme.md');
+foreach ($merge as $key => $value) {
+    $data[$value['Id']]['Titulo'][] = $value['Titulo'];
+    $data[$value['Id']]['Description'][] = $value['Description'];
+    
 }
 //Merge the E1/E2/E3 fields
 $arrayJson = array_merge_recursive($masterArray[0],$masterArray[1],$masterArray[2],$masterArray[3],$masterArray[4],$masterArray[5]);
