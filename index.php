a7421d20bed488a79e7699f811a1b1e24927075c
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-09-13 18:56:38 +0200

    code identation

--- index.php
+++ index.php
@@ -3,18 +3,18 @@
 use OrbitaDigital\Read\ReadCsv;
 use OrbitaDigital\Read\Resources;
 
-require_once __DIR__ .'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-if(!defined('_PS_VERSION_')){
+if (!defined('_PS_VERSION_')) {
     require_once '../../config/config.inc.php';
     require_once '../../init.php';
 }
 
 $data_lang = [
-     1 => ReadCsv::ReadCsv('data_en.csv'),
-     2 => ReadCsv::ReadCsv('data_fr.csv'),
-     3 => ReadCsv::ReadCsv('data_es.csv'),
-     4 => ReadCsv::ReadCsv('data_pt.csv'),
+    1 => ReadCsv::ReadCsv('rates/data_en.csv'),
+    2 => ReadCsv::ReadCsv('rates/data_fr.csv'),
+    3 => ReadCsv::ReadCsv('rates/data_es.csv'),
+    4 => ReadCsv::ReadCsv('rates/data_pt.csv'),
 ];
 
 if(Resources::checkData($data_lang) != false){
@@ -35,5 +35,3 @@ if(Resources::checkData($data_lang) != false){
 else{
     echo "Check the files again";
 }
-
-
