94be13bc2a629e0cd6ee252393822c75de9c2662
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-09-14 11:48:54 +0200

    created new index for today, 14 september

---
+++ index14_sep.php
@@ -0,0 +1,13 @@
+<?php
+
+use OrbitaDigital\Read\ReadCsv;
+use OrbitaDigital\Read\Resources;
+use OrbitaDigital\Read\ReadJson;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+if (!defined('_PS_VERSION_')) {
+    require_once '../../config/config.inc.php';
+    require_once '../../init.php';
+}
+
