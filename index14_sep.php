6ab57638976216c33ba3b3117ceed3e0ec69dc06
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-09-14 11:49:47 +0200

    added new files and compared with previous one

--- index14_sep.php
+++ index14_sep.php
@@ -11,3 +11,19 @@ if (!defined('_PS_VERSION_')) {
     require_once '../../init.php';
 }
 
+$data_new_lang = [
+    1 => ReadCsv::readCSV('rates/data_en_2.csv'),
+    2 => ReadCsv::readCSV('rates/data_fr_2.csv'),
+    3 => ReadCsv::readCSV('rates/data_es_2.csv'),
+    4 => ReadCsv::readCSV('rates/data_pt_2.csv'),
+];
+
+if (Resources::checkCsvData($data_new_lang) != false) {
+    $csvData = Resources::processCsvArray($data_new_lang);
+} else {
+    echo "Check the files again";
+}
+$readed = json_decode(ReadJson::readJson('rates_processed/data_13_Sep_2021.json'),true);
+
+Resources::showJsonData($readed);
+Resources::compareJsonData($csvData,$readed);
