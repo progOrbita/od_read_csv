7c15f45d22d9d8becac611942d800b7012138bff
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-09-14 16:27:16 +0200

    created class for readFiles for general comprobations

---
+++ src/ReadFiles.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OrbitaDigital\Read;
+
+class ReadFiles
+{
+    private $lastError = '';
+    /**
+     * Check if file exist, can be readed and extension is right
+     * @param string $file file to be checked
+     * @param string $extension extension of the file
+     * @return bool true if there's no error. False otherwise
+     */
+    protected function checkFile(string $file, string $extension): bool
+    {
+        if (!file_exists($file)) {
+            $this->lastError = "<b>" . $file . "</b> doesn't exist<br/>";
+            return false;
+        }
+        if (!is_readable($file)) {
+            $this->lastError = "<b>" . $file . "</b> couldn't be read<br/>";
+            return false;
+        }
+        if (preg_match('/^.+\.'.$extension.'/i', $file) <= 0) {
+            $this->lastError = "<b>" . $file . "</b> isn't a ".$extension." file<br/>";
+            return false;
+        }
+        return true;
+    }
+    /**
+     * Get the information of the error from the checks.
+     */
+    protected function getError(){
+        return $this->lastError;
+    }
+}
