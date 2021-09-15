ef37c2846a0af83062bd4d68dd61e989912411e6
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-09-15 11:59:10 +0200

    findErrors to readFiles

--- src/ReadFiles.php
+++ src/ReadFiles.php
@@ -36,5 +36,30 @@ class ReadFiles
     protected function getError(): string
     {
         return self::$lastError;
+    /**
+     * Compare two arrays to find distinct values beetwen both
+     * @param array $rightArray json which is fine
+     * @param array $arrayToCompare csv array which may contains errors and will return them if the value is different
+     * @return bool|array $errorArrays with the errors located beetwen both files. False if keys differs.
+     */
+    public static function findErrors(array $rightArray, array $arrayToCompare)
+    {
+
+        $dataError = [];
+        foreach ($rightArray as $id => $value) {
+            if (array_keys($rightArray[$id]) === array_keys($arrayToCompare[$id])) {
+                for ($id_lang = 1; $id_lang <= 4; $id_lang++) {
+                    if ($rightArray[$id]['Titulo'][$id_lang] !== $arrayToCompare[$id]['Titulo'][$id_lang]) {
+                        $dataError[$id]['Titulo'][$id_lang][] = $arrayToCompare[$id]['Titulo'][$id_lang];
+                    }
+                    if ($rightArray[$id]['Description'][$id_lang] !== $arrayToCompare[$id]['Description'][$id_lang]) {
+                        $dataError[$id]['Description'][$id_lang] = $arrayToCompare[$id]['Description'][$id_lang];
+                    }
+                }
+            } else {
+                return false;
+            }
+        }
+        return $dataError;
     }
 }
--- src/Resources.php
+++ src/Resources.php
@@ -1,34 +1,3 @@
 <?php
 
 namespace OrbitaDigital\Read;
-
-class Resources
-{
-
-    /**
-     * Compare two arrays to find distinct values beetwen both
-     * @param array $rightArray json which is fine
-     * @param array $arrayToCompare json which may contains errors and will return them if the value is different
-     * @return bool|array $errorArrays with the errors located beetwen both files. False if keys differs.
-     */
-    public static function findErrors(array $rightArray, array $arrayToCompare)
-    {
-
-        $dataError = [];
-        foreach ($rightArray as $id => $value) {
-            if (array_keys($rightArray[$id]) === array_keys($arrayToCompare[$id])) {
-                for ($id_lang = 1; $id_lang <= 4; $id_lang++) {
-                    if ($rightArray[$id]['Titulo'][$id_lang] !== $arrayToCompare[$id]['Titulo'][$id_lang]) {
-                        $dataError[$id]['Titulo'][$id_lang][] = $arrayToCompare[$id]['Titulo'][$id_lang];
-                    }
-                    if ($rightArray[$id]['Description'][$id_lang] !== $arrayToCompare[$id]['Description'][$id_lang]) {
-                        $dataError[$id]['Description'][$id_lang] = $arrayToCompare[$id]['Description'][$id_lang];
-                    }
-                }
-            } else {
-                return false;
-            }
-        }
-        return $dataError;
-    }
-}
