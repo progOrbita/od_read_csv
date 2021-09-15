c254027742e6de8618780bd5bebf5ee5b77204ea
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-09-15 09:21:52 +0200

    renamed file to obtain the date

---
+++ index_14_sep_2021.php
@@ -0,0 +1,26 @@
+<?php
+
+use OrbitaDigital\Read\Csv;
+use OrbitaDigital\Read\Json;
+use OrbitaDigital\Read\Resources;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$data_new_lang = [
+    1 => Csv::read('rates/data_en_2.csv'),
+    2 => Csv::read('rates/data_fr_2.csv'),
+    3 => Csv::read('rates/data_es_2.csv'),
+    4 => Csv::read('rates/data_pt_2.csv'),
+];
+
+$csvData = Resources::processCsvArray($data_new_lang);
+$jsonFile = Json::readJson('rates_processed/data_'.$date.'.json');
+$jsonReaded = json_decode($jsonFile, true);
+
+$dataError = Resources::findErrors($jsonReaded, $csvData);
+
+if ($dataError === false) {
+    die('Keys dont match, array cant be compared');
+} else {
+    echo '<br/>Error founds, '. Json::dataToFile($dataError);
+}
