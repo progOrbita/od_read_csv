2814ce0b80e3d0e1f5d4139abca504af93087087
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-09-20 13:25:36 +0200

    modified index to work with both actions

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 $jsonReader = new Json();
 $csvReader = new Csv(['Id', 'Titulo', 'Description']);
 
-$date = date('d_M_Y');
 $data_lang = [];
 $data_new_lang = [];
 $csvLang = [1 => 'rates/data_en.csv', 2 => 'rates/data_fr.csv', 3 => 'rates/data_es.csv', 4 => 'rates/data_pt.csv'];
@@ -24,21 +23,21 @@ if (isset($_GET['add'])) {
     echo 'Attempting to add data...<br/>';
     $csvData = $csvReader->process($data_lang);
 
-    if ($jsonReader->save($csvData, 'data')) {
-        die($jsonReader->getMessage());
+    if (!$jsonReader->save($csvData, 'data')) {
+        die($jsonReader->getLastError());
     }
-    die($jsonReader->getLastError());
+    echo($jsonReader->getMessage());
 }
 
 if (isset($_GET['update'])) {
-    echo 'checking errors...<br/>';
+    echo '<br/>checking errors...<br/>';
 
     foreach ($csv_new_lang as $id_lang =>  $file) {
 
         $data_new_file = $csvReader->read($file);
         $data_new_file ? $data_new_lang[$id_lang] = $data_new_file : die($csvReader->getLastError());
     }
-
+    $date = date('d_M_Y');
     $jsonData = $jsonReader->read('rates_processed/data_' . $date . '.json');
 
     if (!$jsonData) {
