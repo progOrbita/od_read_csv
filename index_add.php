67b49b9675dcce564a8ab1c9888c1ec8a7a4ccbc
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-09-20 19:23:34 +0200

    php script which adds a csv array into a json file

---
+++ index_add.php
@@ -0,0 +1,24 @@
+<?php
+use OrbitaDigital\Read\Csv;
+use OrbitaDigital\Read\Json;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$jsonReader = new Json();
+$csvReader = new Csv(['Id', 'Titulo', 'Description']);
+
+$data_lang = [];
+$csvLang = [1 => 'rates/data_en.csv', 2 => 'rates/data_fr.csv', 3 => 'rates/data_es.csv', 4 => 'rates/data_pt.csv'];
+
+foreach ($csvLang as $id_lang =>  $file) {
+    
+    $data_file = $csvReader->read($file);
+    $data_file ? $data_lang[$id_lang] = $data_file : die($csvReader->getLastError());
+}
+
+$csvData = $csvReader->process($data_lang);
+
+if ($jsonReader->save($csvData, 'data')) {
+    die($jsonReader->getMessage());
+}
+die($jsonReader->getLastError());
